09abc898673a29a0c6abae0b90ddf747d1c736af
Yigit can Bölükbaşı <yigitcanbolukbasi@Yigit-MacBook-Air.local>
2022-05-27 11:21:59 +0300

    offer card css fixed

--- src/Components/OfferCard/index.js
+++ src/Components/OfferCard/index.js
@@ -1,14 +1,56 @@
 import React from "react";
+import { Button } from "@mui/material";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
 
 function Card({ offerOne }) {
   console.log(offerOne);
   return (
-    <div>
-      <img src={offerOne.ImagePath} alt="photo" />
-      <h3>Firma Adı:{offerOne.FirmName}</h3>
-      <p>cash:{offerOne.Cash}</p>
-      <h5>{offerOne.ProductDesc}</h5>
-    </div>
+    <Box sx={{ flexGrow: 1 }}>
+      <div
+        style={{ border: "solid", margin: 10, borderRadius: 5, padding: 10 }}
+      >
+        <Grid container spacing={0}>
+          <Grid
+            md={2}
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <img src={offerOne.ImagePath} alt="photo" />
+          </Grid>
+          <Grid
+            md={8}
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "flex-start",
+              alignItems: "flex-start",
+              borderLeft: "solid",
+              paddingLeft: 50,
+            }}
+          >
+            <h5>{offerOne.ProductDesc}</h5>
+            <h3>{offerOne.FirmName}</h3>
+          </Grid>
+          <Grid
+            md={2}
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "flex-end",
+              alignItems: "end",
+              padding: 10,
+            }}
+          >
+            <h3>{offerOne.Cash} TL</h3>
+            <Button variant="contained">Satın Al</Button>
+          </Grid>
+        </Grid>
+      </div>
+    </Box>
   );
 }
 
